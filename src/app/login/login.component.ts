bff11f03eff77be42fdd0bf9b7549f4cf67af948
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-23 17:47:21 +0100

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../services/auth.service';
+import { LoginEntity } from '../model/LoginEntity';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loginData: LoginEntity;
+  constructor(private auth: AuthService, private router: Router) {
+    this.loginData = {
+      username: '',
+      password: '',
+    };
   }
-
+  submitForm() {
+    const correctLogin = this.auth.login(this.loginData);
+    if (correctLogin) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+  ngOnInit(): void {}
 }
