ed7049a827bd5fc4eb242bc75143a2f1e39b7f43
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-23 17:47:32 +0100

    inyect auth Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'app';
+  constructor(public auth: AuthService) {}
 }
