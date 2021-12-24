60177c8a0d4b28b1d851a0fe978623971519dd23
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-24 15:12:24 +0100

    change routes and initial navigation

--- src/app/menu/menu-public/menu-public.component.ts
+++ src/app/menu/menu-public/menu-public.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-public',
   templateUrl: './menu-public.component.html',
-  styleUrls: ['./menu-public.component.scss']
+  styleUrls: ['./menu-public.component.scss'],
 })
 export class MenuPublicComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.navigate(['/home']);
   }
-
 }
