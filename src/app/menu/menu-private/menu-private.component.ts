376b58fde610accb6286ef34ece6d36830eb2ac5
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-12-24 10:00:24 +0100

    add userLogged and logut button to private menu component

--- src/app/menu/menu-private/menu-private.component.ts
+++ src/app/menu/menu-private/menu-private.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-menu-private',
   templateUrl: './menu-private.component.html',
-  styleUrls: ['./menu-private.component.scss']
+  styleUrls: ['./menu-private.component.scss'],
 })
 export class MenuPrivateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loggedUser: string = '';
+  constructor(public auth: AuthService, private router: Router) {}
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['']);
   }
-
+  ngOnInit(): void {
+    if (this.auth.isLogged()) {
+      this.loggedUser = this.auth.getUsername().split('@')[0];
+      this.router.navigate(['/dashboard']);
+    }
+  }
 }
